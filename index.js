855cb4bac0156c3a73a25ecdbd3097f10bd1aee7
정호연 <jea7730@gmail.com>
2024-06-13 16:53:59 +0900

    api url 조립

---
+++ index.js
@@ -0,0 +1,5 @@
+const keyName = `n8uIBadsqMx4UqYvGKL7l7l2Gkut99sQyvUHXQJdNhOo0pQQRE0vwEgMMYsptCZ91a4L%2Fna8hWLVrGmNkOQS5w%3D%3D`;
+
+const endPoint = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${keyName}&numOfRows=10&pageNo=1&dataType=JSON&base_date=20240612&base_time=1400&nx=67&ny=100`;
+
+console.log(endPoint);
