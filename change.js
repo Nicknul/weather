f41f7a8adea66d72051c372985af971bd6cd20ee
정호연 <jea7730@gmail.com>
2024-06-13 17:31:48 +0900

    for문을 통해 데이터 추출 완료

--- change.js
+++ change.js
@@ -4,9 +4,15 @@ let data = fs.readFileSync('./utf-8.csv', 'utf-8');
 
 // enter 공백 없애기
 let splitData = data.split('\r\n');
-// obj의 key data 뽑기
+// obj의 key가 될 data 추출
 let title = splitData[0].split(',');
 
+// for문을 통해 title를 제외한 나머지 값 나타나게 하기
 for (let i = 1; i < splitData.length; i++) {
-  // console.log(splitData[i]);
+  // ,(쉼표) 제거
+  let a = splitData[i].split(',');
+  // console.log(a);
+  for (let j = 0; j < title.length; j++) {
+    // console.log(splitData[j]);
+  }
 }
