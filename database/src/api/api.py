b46057804d5049c262536b14c9c833416c6154f6
정호연 <jea7730@gmail.com>
2024-08-16 11:14:34 +0900

    :rocket: 정호연 : 기상청 중기기온조회 api 받아오기

---
+++ database/src/api/api.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+api_key = 'n8uIBadsqMx4UqYvGKL7l7l2Gkut99sQyvUHXQJdNhOo0pQQRE0vwEgMMYsptCZ91a4L%2Fna8hWLVrGmNkOQS5w%3D%3D'
+url = f'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey={api_key}&numOfRows=10&pageNo=1&dataType=JSON&regId=11C20401&tmFc=202408160600'
+
+response = requests.get(url)
+data = response.json()
+
+pretty_data = json.dumps(data, indent=2)
+
+print(pretty_data)
