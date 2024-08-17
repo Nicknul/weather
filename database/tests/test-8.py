9c2eca1edfe8c3839b2386ce56d37f102b025bd3
정호연 <jea7730@gmail.com>
2024-08-17 21:10:21 +0900

    :construction_worker: 정호연 : __name__ 사용 예제

---
+++ database/tests/test-8.py
@@ -0,0 +1,12 @@
+def greet():
+    print("Hello, World!")
+
+def farewell():
+    print("Goodbye, World!")
+
+def main():
+    greet()  # 메인 함수에서 다른 함수 호출
+    farewell()
+
+if __name__ == "__main__":
+    main()  # 프로그램이 직접 실행될 때 main 함수 호출
