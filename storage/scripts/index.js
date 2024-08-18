94d5d405efbf16a8ebf7e9634b68db86576b78f3
정호연 <jea7730@gmail.com>
2024-08-18 17:28:27 +0900

    :white_check_mark: 정호연 : api data 가져오기 테스트

--- storage/scripts/index.js
+++ storage/scripts/index.js
@@ -1,5 +1,11 @@
 const keyName = `n8uIBadsqMx4UqYvGKL7l7l2Gkut99sQyvUHXQJdNhOo0pQQRE0vwEgMMYsptCZ91a4L%2Fna8hWLVrGmNkOQS5w%3D%3D`;
 
-const endPoint = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${keyName}&numOfRows=10&pageNo=1&dataType=JSON&base_date=20240612&base_time=1400&nx=67&ny=100`;
+const endPoint = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${keyName}&numOfRows=10&pageNo=1&dataType=JSON&base_date=20240817&base_time=1400&nx=67&ny=100`;
 
-console.log(endPoint);
+async function weather() {
+  let a = await fetch(endPoint);
+  let b = await a.json();
+  console.log(b.response.body.items);
+}
+
+weather();
