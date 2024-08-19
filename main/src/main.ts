bd62c33ce8ead21325719108e3093f726cff6dc7
정호연 <jea7730@gmail.com>
2024-08-19 23:12:58 +0900

    :bricks: 정호연 : CORS 설정 추가

--- main/src/main.ts
+++ main/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // CORS 활성화
+  app.enableCors({
+    origin: 'http://localhost:3000', // 프론트엔드의 주소
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(8000); // 포트 8000번 사용
 }
 bootstrap();
