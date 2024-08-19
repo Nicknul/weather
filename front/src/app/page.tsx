a1f27302fbd5fdd975d0c6eed8c159b91351f56e
정호연 <jea7730@gmail.com>
2024-08-19 23:05:52 +0900

    :white_check_mark: 정호연 : button component 조립

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
+import RequestButton from './components/RequestButton';
 
 export default function Home() {
   return (
     <>
       <div className="m-5 bg-blue">
         <button className="bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700">Hello World</button>
+        <h1>프론트에서 메인 서버로 요청 보내기</h1>
+        <RequestButton />
       </div>
     </>
   );
