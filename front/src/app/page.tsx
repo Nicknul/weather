8c35cb54530351e1bccacd210e8f0269de1374d1
정호연 <jea7730@gmail.com>
2024-08-19 21:50:40 +0900

    :white_check_mark: 정호연 : Next.js 브라우저 테스트

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,8 +3,8 @@
 export default function Home() {
   return (
     <>
-      <div className="m-5">
-        <button className="bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700">hello world</button>
+      <div className="m-5 bg-blue">
+        <button className="bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700">Hello World</button>
       </div>
     </>
   );
