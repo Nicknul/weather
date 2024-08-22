68be19bbe35a5b7981ef3e287a8f98f3eba43f9d
정호연 <jea7730@gmail.com>
2024-08-22 21:43:42 +0900

    :recycle: 정호연 : 코드 정리

--- database/src/api/api.py
+++ database/src/api/api.py
@@ -1,13 +1,13 @@
 import requests
+import json
 from ..components.date import now_datatime
-import pprint
 
 api_key = 'n8uIBadsqMx4UqYvGKL7l7l2Gkut99sQyvUHXQJdNhOo0pQQRE0vwEgMMYsptCZ91a4L%2Fna8hWLVrGmNkOQS5w%3D%3D'
-url = f'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey={api_key}&numOfRows=10&pageNo=1&dataType=JSON&regId=11C20401&tmFc=202408190600'
+url = f'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey={api_key}&numOfRows=10&pageNo=1&dataType=JSON&regId=11C20401&tmFc=202408220600'
 
 response = requests.get(url)
 data = response.json()
+pretty_data = json.dumps(data, indent=2)
+weather_data = data['response']['body']['items']['item']
 
-pprint.pprint(data['response']['body']['items']['item'], indent=2)
-
-# print(data['response'])
+print(pretty_data)
