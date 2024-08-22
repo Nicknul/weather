e1b822e351a75a01f49c274e56e7029c0d3b4350
정호연 <jea7730@gmail.com>
2024-08-22 22:25:00 +0900

    :sparkles: 정호연 : 중기예보 data 가져오기

--- database/src/api/api.py
+++ database/src/api/api.py
@@ -1,13 +1,18 @@
 import requests
 import json
+from fastapi import APIRouter
 from ..components.date import now_datatime
 
-api_key = 'n8uIBadsqMx4UqYvGKL7l7l2Gkut99sQyvUHXQJdNhOo0pQQRE0vwEgMMYsptCZ91a4L%2Fna8hWLVrGmNkOQS5w%3D%3D'
-url = f'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey={api_key}&numOfRows=10&pageNo=1&dataType=JSON&regId=11C20401&tmFc=202408220600'
+weather_router = APIRouter()
+@weather_router.get('/weather')
+def get_weather_data():
+  api_key = 'n8uIBadsqMx4UqYvGKL7l7l2Gkut99sQyvUHXQJdNhOo0pQQRE0vwEgMMYsptCZ91a4L%2Fna8hWLVrGmNkOQS5w%3D%3D'
+  url = f'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey={api_key}&numOfRows=10&pageNo=1&dataType=JSON&regId=11C20401&tmFc=202408220600'
 
-response = requests.get(url)
-data = response.json()
-pretty_data = json.dumps(data, indent=2)
-weather_data = data['response']['body']['items']['item']
+  response = requests.get(url)
+  data = response.json()
+  pretty_data = json.dumps(data, indent=2)
+  weather_data = data['response']['body']['items']['item']
 
-print(pretty_data)
+  print(pretty_data)
+  return weather_data
