b0da89532bc29bef094a59f831a71f57ea5f8d0b
정호연 <jea7730@gmail.com>
2024-08-22 09:57:32 +0900

    :adhesive_bandage: 정호연 : catch 에러 콘솔 추가

--- front/src/app/components/RequestButton.tsx
+++ front/src/app/components/RequestButton.tsx
@@ -3,15 +3,14 @@ import { fetchFromServer } from '../services/apiClient';
 const RequestButton = () => {
   const handleClick = async () => {
     try {
-      const data = await fetchFromServer();
-      console.log('프론트: 메인 서버로부터 정상적인 응답을 받았습니다.', data);
+      await fetchFromServer();
     } catch (error) {
-      console.error('프론트: 요청 중 에러 발생:', error);
+      console.error('프론트: 버튼 클릭 처리 중 에러 발생:', error);
     }
   };
 
   return (
-    <button onClick={handleClick} className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
+    <button className="bg-blue-600 py-2 px-4 rounded-lg text-white hover:bg-blue-700" onClick={handleClick}>
       서버에 요청 보내기
     </button>
   );
