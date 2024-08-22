ccdc09f98fb08b68c9bad87dff4b9b2bdf716a69
정호연 <jea7730@gmail.com>
2024-08-22 22:15:47 +0900

    :green_heart: 정호연 : api_router 조립

--- database/main.py
+++ database/main.py
@@ -1,13 +1,11 @@
-from fastapi import FastAPI, HTTPException
+# src/main.py
+from fastapi import FastAPI
+from src.api.api import weather_router
 
 app = FastAPI()
 
-@app.get("/db")
-async def get_data():
-    try:
-        data = {"message": "데이터베이스 서버에서 온 응답입니다."}
-        print("데이터베이스 서버: 요청을 받고 응답을 처리했습니다.")
-        return data
-    except Exception as e:
-        print(f"데이터베이스 서버: 요청 처리 중 에러 발생: {e}")
-        raise HTTPException(status_code=500, detail="데이터베이스 서버: 요청 처리 실패")
+app.include_router(weather_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
