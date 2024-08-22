6a7f17df5144f96350152bb080cfe23110b9d228
정호연 <jea7730@gmail.com>
2024-08-22 15:03:43 +0900

    :fire: 정호연 : 주석 삭제

--- database/main.py
+++ database/main.py
@@ -5,7 +5,6 @@ app = FastAPI()
 @app.get("/db")
 async def get_data():
     try:
-        # 데이터 처리 또는 데이터베이스 접근 로직
         data = {"message": "데이터베이스 서버에서 온 응답입니다."}
         print("데이터베이스 서버: 요청을 받고 응답을 처리했습니다.")
         return data
