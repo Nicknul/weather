e3fc083ba282b9142728599806f7f2d1f5d9fa20
정호연 <jea7730@gmail.com>
2024-08-22 22:24:18 +0900

    :green_heart: 정호연 : WeatherButton 조립

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import RequestButton from './components/RequestButton';
+import WeatherButton from './components/WeatherButton';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <button className="bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700">Hello World</button>
         <h1>프론트에서 메인 서버로 요청 보내기</h1>
         <RequestButton />
+        <WeatherButton />
       </div>
     </>
   );
