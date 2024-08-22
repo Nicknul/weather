d5a4776e9af2dcadce51fe2953db29869a44d83a
정호연 <jea7730@gmail.com>
2024-08-22 09:57:40 +0900

    :adhesive_bandage: 정호연 : catch 에러 콘솔 추가

--- front/src/app/services/apiClient.ts
+++ front/src/app/services/apiClient.ts
@@ -5,13 +5,14 @@ export const fetchFromServer = async () => {
     });
 
     if (!response.ok) {
-      throw new Error(`서버 응답 실패. 상태 코드: ${response.status}`);
+      throw new Error(`메인 서버 응답 실패. 상태 코드: ${response.status}`);
     }
 
     const data = await response.json();
+    console.log('프론트: 메인 서버로부터 응답을 받았습니다.', data);
     return data;
   } catch (error) {
-    console.error('apiClient: 요청 중 에러 발생:', error);
+    console.error('프론트: 메인 서버 요청 중 에러 발생:', error);
     throw error;
   }
 };
