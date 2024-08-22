f2f070fa09ea721d7ed843068d33ecd9abc3d40f
정호연 <jea7730@gmail.com>
2024-08-22 09:56:29 +0900

    :globe_with_meridians: 정호연 : main > database server

---
+++ database/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+@app.get("/db")
+async def get_data():
+    try:
+        # 데이터 처리 또는 데이터베이스 접근 로직
+        data = {"message": "데이터베이스 서버에서 온 응답입니다."}
+        print("데이터베이스 서버: 요청을 받고 응답을 처리했습니다.")
+        return data
+    except Exception as e:
+        print(f"데이터베이스 서버: 요청 처리 중 에러 발생: {e}")
+        raise HTTPException(status_code=500, detail="데이터베이스 서버: 요청 처리 실패")
