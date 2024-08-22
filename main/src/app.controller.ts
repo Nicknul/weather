61b6d737df804b02ab85b99a6eccf3fb4042ae06
정호연 <jea7730@gmail.com>
2024-08-22 09:59:00 +0900

    :globe_with_meridians: 정호연 : front > main > database 요청, 응답

--- main/src/app.controller.ts
+++ main/src/app.controller.ts
@@ -1,14 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get('test')
-  handleTestRequest() {
+  async handleTestRequest() {
     console.log('메인 서버: 프론트로부터 요청을 받았습니다.');
 
-    return { message: '메인 서버에서 응답을 전송합니다.' };
+    try {
+      const response = await fetch('http://localhost:8080/db', {
+        method: 'GET',
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `데이터베이스 서버 응답 실패. 상태 코드: ${response.status}`,
+        );
+      }
+
+      const data = await response.json();
+      console.log(
+        '메인 서버: 데이터베이스 서버로부터 응답을 받았습니다.',
+        data,
+      );
+      return data;
+    } catch (error) {
+      console.error('메인 서버: 데이터베이스 서버 요청 중 에러 발생:', error);
+      throw new Error('메인 서버: 데이터베이스 서버 요청 실패');
+    }
   }
 }
