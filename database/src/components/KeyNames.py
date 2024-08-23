18bf05c358bd5ede96dc0414b1a7b1790949a10e
정호연 <jea7730@gmail.com>
2024-08-23 23:27:03 +0900

    :fire: 정호연 : 주석 삭제

--- database/src/components/KeyNames.py
+++ database/src/components/KeyNames.py
@@ -1,14 +1,10 @@
 import json
 
-# JSON 파일의 절대 경로를 직접 설정
 json_file_path = '/Users/waddi/Desktop/weather/storage/json_data/ZoneCode.json'
 
-# JSON 파일을 열고 데이터를 읽어오기
 with open(json_file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-# JSON 객체의 키 이름 가져오기
 keys = data.keys()
 
-# 키 이름 출력
 print("Keys:", list(keys))
