7b4ef07c759c892468adf754adbee0437d3e2abf
정호연 <jea7730@gmail.com>
2024-08-23 10:05:51 +0900

    :adhesive_bandage: 정호연 : 변경된 라우터 이름으로 수정

--- database/main.py
+++ database/main.py
@@ -1,10 +1,10 @@
 # src/main.py
 from fastapi import FastAPI
-from src.api.api import weather_router
+from src.api.api import api_router
 
 app = FastAPI()
 
-app.include_router(weather_router)
+app.include_router(api_router)
 
 if __name__ == "__main__":
     import uvicorn
