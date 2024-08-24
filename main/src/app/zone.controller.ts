6af2255f4b18beb5161e8972329efbbb52de6000
정호연 <jea7730@gmail.com>
2024-08-24 18:02:52 +0900

    :globe_with_meridians: 정호연 : 지역 이름 요청, 응답

--- main/src/app/zone.controller.ts
+++ main/src/app/zone.controller.ts
@@ -1,20 +1,25 @@
-// src/app/data.controller.ts
+// src/app/zone.controller.ts
 
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('data')
-export class DataController {
-  @Get('fetch-array')
-  async fetchArrayFromPython() {
-    const response = await fetch('http://localhost:8080/zone-keys', {
-      method: 'GET',
-    });
+@Controller('zone')
+export class ZoneController {
+  @Get('keys')
+  async fetchZoneKeys() {
+    try {
+      const response = await fetch('http://localhost:8080/zone-keys', {
+        method: 'GET',
+      });
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      if (!response.ok) {
+        throw new Error(`Python 서버 응답 실패. 상태 코드: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('메인 서버: Python 서버 요청 중 에러 발생:', error);
+      throw new Error('메인 서버: Python 서버 요청 실패');
     }
-
-    const data = await response.json();
-    return data;
   }
 }
