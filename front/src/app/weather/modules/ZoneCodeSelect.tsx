5665ea3d5edc5c5b6ca1ff223bc82b18ba02974b
정호연 <jea7730@gmail.com>
2024-08-24 18:22:17 +0900

    :zap: 정호연 : finally를 사용하여 데이터를 다 받았다면 응답 닫기

--- front/src/app/weather/modules/ZoneCodeSelect.tsx
+++ front/src/app/weather/modules/ZoneCodeSelect.tsx
@@ -1,9 +1,12 @@
+// src/app/modules/ZoneCodeSelect.tsx
+
 import React, { useState, useEffect } from 'react';
 import SelectBox from '@/modules/SelectBodx';
 import { fetchZoneKeys } from '@/app/services/zoneService';
 
 const ZoneCodeSelect: React.FC = () => {
   const [options, setOptions] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(true); // 로딩 상태 추가
 
   useEffect(() => {
     const loadData = async () => {
@@ -12,6 +15,8 @@ const ZoneCodeSelect: React.FC = () => {
         setOptions(data);
       } catch (error) {
         console.error('ZoneCodeSelect: Error loading zone keys:', error);
+      } finally {
+        setLoading(false); // 데이터 로딩이 완료되면 로딩 상태를 false로 설정
       }
     };
 
@@ -22,7 +27,15 @@ const ZoneCodeSelect: React.FC = () => {
     console.log('Selected zone:', value);
   };
 
-  return <SelectBox options={options} onChange={handleSelectChange} label="지역 코드 선택" />;
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p> // 로딩 중일 때 표시할 요소
+      ) : (
+        <SelectBox options={options} onChange={handleSelectChange} label="지역 코드 선택" />
+      )}
+    </div>
+  );
 };
 
 export default ZoneCodeSelect;
