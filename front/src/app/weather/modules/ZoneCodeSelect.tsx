9d4166f2c2234a5f645133342ccd225d2b37622d
정호연 <jea7730@gmail.com>
2024-08-24 17:18:49 +0900

    :alien: 정호연 : 엔드포인트 수정

--- front/src/app/weather/modules/ZoneCodeSelect.tsx
+++ front/src/app/weather/modules/ZoneCodeSelect.tsx
@@ -8,7 +8,7 @@ const ZoneCodeSelect: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:8000/fetch-array');
+        const response = await fetch('http://localhost:8000/data/fetch-array');
         if (!response.ok) {
           throw new Error(`Failed to fetch data. Status: ${response.status}`);
         }
