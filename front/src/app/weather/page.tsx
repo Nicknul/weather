a2ff63087a556497a277b0f2b8686b58b25b5358
정호연 <jea7730@gmail.com>
2024-08-24 16:08:52 +0900

    :adhesive_bandage: 정호연 : 함수명 수정

--- front/src/app/weather/page.tsx
+++ front/src/app/weather/page.tsx
@@ -2,7 +2,7 @@
 import WeatherButton from './components/WeatherButton';
 import ZoneCodeSelect from './modules/ZoneCodeSelect';
 
-export default function Home() {
+export default function Weather() {
   return (
     <>
       <div className="m-5 bg-blue">
