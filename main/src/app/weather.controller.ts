2d2c71749d24fc8e91e9d0fc50265b11f5beaf85
정호연 <jea7730@gmail.com>
2024-08-24 17:26:29 +0900

    :recycle: 정호연 : weather controller

---
+++ main/src/app/weather.controller.ts
@@ -0,0 +1,33 @@
+// src/app/weather.controller.ts
+
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('weather')
+export class WeatherController {
+  @Get()
+  async handleWeatherRequest() {
+    console.log('메인 서버: 프론트로부터 날씨 요청을 받았습니다.');
+
+    try {
+      const response = await fetch('http://localhost:8080/weather', {
+        method: 'GET',
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `데이터베이스 서버 응답 실패. 상태 코드: ${response.status}`,
+        );
+      }
+
+      const data = await response.json();
+      console.log(
+        '메인 서버: 데이터베이스 서버로부터 날씨 응답을 받았습니다.',
+        data,
+      );
+      return data;
+    } catch (error) {
+      console.error('메인 서버: 데이터베이스 서버 요청 중 에러 발생:', error);
+      throw new Error('메인 서버: 데이터베이스 서버 요청 실패');
+    }
+  }
+}
