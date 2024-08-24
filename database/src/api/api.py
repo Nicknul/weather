1881846fe4a73e03cefe35d4f68a55f721ebf23f
정호연 <jea7730@gmail.com>
2024-08-24 12:53:13 +0900

    :adhesive_bandage: 정호연 : print 추가

--- database/src/api/api.py
+++ database/src/api/api.py
@@ -8,11 +8,12 @@ api_router = APIRouter()
 def get_weather_data():
   api_key = 'n8uIBadsqMx4UqYvGKL7l7l2Gkut99sQyvUHXQJdNhOo0pQQRE0vwEgMMYsptCZ91a4L%2Fna8hWLVrGmNkOQS5w%3D%3D'
   url = f'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey={api_key}&numOfRows=10&pageNo=1&dataType=JSON&regId=11C20401&tmFc={api_datetime}'
+  print(url)
 
   response = requests.get(url)
   data = response.json()
   pretty_data = json.dumps(data, indent=2)
   weather_data = data['response']['body']['items']['item']
 
-  print(pretty_data)
+  # print(pretty_data)
   return weather_data
