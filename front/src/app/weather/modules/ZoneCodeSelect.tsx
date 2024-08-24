eb3188faeb7f6a4dfe266ab85c00c2867e4a9eaf
정호연 <jea7730@gmail.com>
2024-08-24 19:36:00 +0900

    :green_heart: 정호연 : 리펙토링 모듈 조립

--- front/src/app/weather/modules/ZoneCodeSelect.tsx
+++ front/src/app/weather/modules/ZoneCodeSelect.tsx
@@ -1,41 +1,13 @@
 // src/app/modules/ZoneCodeSelect.tsx
 
-import React, { useState, useEffect } from 'react';
-import SelectBox from '@/modules/SelectBodx';
-import { fetchZoneKeys } from '@/app/services/zoneService';
+import React from 'react';
+import SelectBox from '@/modules/SelectBox';
+import { useZoneCode } from '@/app/hooks/useZoneCode';
 
 const ZoneCodeSelect: React.FC = () => {
-  const [options, setOptions] = useState<string[]>([]);
-  const [loading, setLoading] = useState<boolean>(true); // 로딩 상태 추가
+  const { options, handleSelectChange } = useZoneCode();
 
-  useEffect(() => {
-    const loadData = async () => {
-      try {
-        const data = await fetchZoneKeys();
-        setOptions(data);
-      } catch (error) {
-        console.error('ZoneCodeSelect: Error loading zone keys:', error);
-      } finally {
-        setLoading(false); // 데이터 로딩이 완료되면 로딩 상태를 false로 설정
-      }
-    };
-
-    loadData();
-  }, []);
-
-  const handleSelectChange = (value: string) => {
-    console.log('Selected zone:', value);
-  };
-
-  return (
-    <div>
-      {loading ? (
-        <p>Loading...</p> // 로딩 중일 때 표시할 요소
-      ) : (
-        <SelectBox options={options} onChange={handleSelectChange} label="지역 코드 선택" />
-      )}
-    </div>
-  );
+  return <SelectBox options={options} onChange={handleSelectChange} label="지역 코드 선택" />;
 };
 
 export default ZoneCodeSelect;
