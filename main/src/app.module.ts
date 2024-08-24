b70890995a9faa0a8d52a46858bdcf82226ef18d
정호연 <jea7730@gmail.com>
2024-08-24 18:03:14 +0900

    :alien: 정호연 : 파일명 변경으로 인한 수정

--- main/src/app.module.ts
+++ main/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WeatherController } from './app/weather.controller';
-import { DataController } from './app/data.controller';
+import { ZoneController } from './app/zone.controller';
 
 @Module({
   imports: [],
-  controllers: [WeatherController, DataController],
+  controllers: [WeatherController, ZoneController],
   providers: [],
 })
 export class AppModule {}
