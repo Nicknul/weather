7cdff51c934b5875ceebbf0a04cfe47bb663a268
정호연 <jea7730@gmail.com>
2024-08-24 19:57:34 +0900

    :sparkles: 정호연 : ZoneCode.json에서 일치하는 키 이름이 있는지 확인

--- database/src/components/zone.py
+++ database/src/components/zone.py
@@ -2,10 +2,10 @@ import json
 from fastapi import APIRouter, Request
 
 zone_router = APIRouter()
+json_file_path = '/Users/waddi/Desktop/weather/storage/json_data/ZoneCode.json'
 
 @zone_router.get("/zone-keys")
 def get_zone_keys():
-    json_file_path = '/Users/waddi/Desktop/weather/storage/json_data/ZoneCode.json'
     
     with open(json_file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
@@ -20,7 +20,16 @@ async def receive_zone_data(request: Request):
         zone = data.get('zone')
         print(f"데이터베이스 서버: 메인 서버로부터 받은 지역 코드: {zone}")
 
-        return {"message": "지역 코드를 정상적으로 처리했습니다."}
+        with open(json_file_path, 'r', encoding='utf-8') as file:
+            zone_data = json.load(file)
+
+        if zone in zone_data:
+            print(f"데이터베이스 서버: {zone} 지역 코드가 ZoneCode.json 파일에 존재합니다.")
+            return {"message": f"지역 코드 '{zone}'가 ZoneCode.json에 존재합니다."}
+        else:
+            print(f"데이터베이스 서버: {zone} 지역 코드는 ZoneCode.json 파일에 존재하지 않습니다.")
+            return {"message": f"지역 코드 '{zone}'는 ZoneCode.json에 존재하지 않습니다."}, 404
+
     except Exception as e:
         print(f"데이터베이스 서버: 에러 발생 - {e}")
         return {"message": "지역 코드 처리 중 오류가 발생했습니다."}, 500
