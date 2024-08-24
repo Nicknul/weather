a53101fd291c05b6337a8c1d6ce545a1de210497
정호연 <jea7730@gmail.com>
2024-08-24 17:56:13 +0900

    :sparkles: 정호연 : router 연결

--- database/src/components/zone.py
+++ database/src/components/zone.py
@@ -1,10 +1,14 @@
 import json
+from fastapi import APIRouter
 
-json_file_path = '/Users/waddi/Desktop/weather/storage/json_data/ZoneCode.json'
+zone_router = APIRouter()
 
-with open(json_file_path, 'r', encoding='utf-8') as file:
-    data = json.load(file)
-
-keys = data.keys()
-
-print("Keys:", list(keys))
+@zone_router.get("/zone-keys")
+def get_zone_keys():
+    json_file_path = '/Users/waddi/Desktop/weather/storage/json_data/ZoneCode.json'
+    
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    keys = data.keys()
+    return list(keys)
