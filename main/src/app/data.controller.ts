fa5c421700f97ba600ba0a0f90e2d55c20adaa76
정호연 <jea7730@gmail.com>
2024-08-24 17:26:13 +0900

    :recycle: 정호연 : data controller

---
+++ main/src/app/data.controller.ts
@@ -0,0 +1,20 @@
+// src/app/data.controller.ts
+
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('data')
+export class DataController {
+  @Get('fetch-array')
+  async fetchArrayFromPython() {
+    const response = await fetch('http://localhost:8080/get-array', {
+      method: 'GET',
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data. Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  }
+}
