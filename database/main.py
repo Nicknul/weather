64f5827c613756321a5323dffa60769a8044a035
정호연 <jea7730@gmail.com>
2024-08-24 17:05:45 +0900

    :sparkles: 정호연 : 브라우저가 렌더될 때 배열을 응답함

--- database/main.py
+++ database/main.py
@@ -1,10 +1,25 @@
 # src/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.api import api_router
+
 
 app = FastAPI()
 
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/get-array")
+def get_array():
+    my_array = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+    return my_array
 
 if __name__ == "__main__":
     import uvicorn
