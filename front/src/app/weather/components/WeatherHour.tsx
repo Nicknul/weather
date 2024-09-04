e81b6708353b448d798ae7e219256a42ff0a6a55
정호연 <jea7730@gmail.com>
2024-09-04 17:13:42 +0900

    :adhesive_bandage: 정호연 : 03시 > 3시로 수정

--- front/src/app/weather/components/WeatherHour.tsx
+++ front/src/app/weather/components/WeatherHour.tsx
@@ -6,12 +6,17 @@ const WeatherHour = ({ data }: { data: any[] }) => {
 
   return (
     <div>
-      {hourlyData.map((hour, index) => (
-        <div key={index} className="hourly">
-          <div>{hour.fcstTime.slice(0, 2)}시</div>
-          <div>{hour.fcstValue}℃</div>
-        </div>
-      ))}
+      {hourlyData.map((hour, index) => {
+        // fcstTime의 앞 2자리를 가져와서 숫자로 변환
+        const hourNumber = Number(hour.fcstTime.slice(0, 2));
+
+        return (
+          <div key={index} className="hourly">
+            <div>{hourNumber}시</div>
+            <div>{hour.fcstValue}℃</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
