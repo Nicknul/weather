11d622a4188b4235d91cd55d987d7d631d0b97af
정호연 <jea7730@gmail.com>
2024-09-04 22:10:56 +0900

    :hammer: 정호연 : 동적 처리된 데이터 받은 후 브라우저에 나타나도록 함

--- front/src/app/weather/page.tsx
+++ front/src/app/weather/page.tsx
@@ -1,25 +1,22 @@
+// weather/Weather.tsx
 'use client';
-import React, { useEffect, useState } from 'react';
-import { useWeather } from '../hooks/useWeather';
+import React from 'react';
+import { useWeatherData } from '../hooks/useWeatherData';
 import WeatherDisplay from './components/WeatherDisplay';
-import { getCurrentDate } from '../utils/dateUtils';
 
 const Weather = () => {
-  const [currentDate, setCurrentDate] = useState<string>('');
-
-  // useEffect를 사용해 현재 날짜를 설정하는 로직
-  useEffect(() => {
-    const formattedDate = getCurrentDate(); // 유틸리티 함수 호출
-    setCurrentDate(formattedDate); // 현재 날짜 상태로 설정
-  }, []); // 컴포넌트가 처음 로드될 때 한 번만 실행
-
-  // 현재 날짜를 사용해 날씨 데이터 조회
-  const weatherData = useWeather(currentDate);
+  const { weatherData, locationError, error, isLoading } = useWeatherData();
 
   return (
     <div>
       <h1>날씨 정보</h1>
-      {weatherData.length > 0 ? <WeatherDisplay weatherData={weatherData} /> : <div>데이터를 불러오는 중...</div>}
+      {locationError && <p>{locationError}</p>}
+      {error && <p>{error}</p>}
+      {isLoading ? (
+        <p>위치 정보를 불러오는 중...</p>
+      ) : (
+        weatherData.length > 0 && <WeatherDisplay weatherData={weatherData} />
+      )}
     </div>
   );
 };
