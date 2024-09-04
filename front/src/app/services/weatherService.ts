b7617709fee1b8b87ce47fd5579f9935d6e63bea
정호연 <jea7730@gmail.com>
2024-09-04 20:02:44 +0900

    :green_heart: 정호연 : timeUtils 컴포넌트 조립

--- front/src/app/services/weatherService.ts
+++ front/src/app/services/weatherService.ts
@@ -1,20 +1,21 @@
 // services/weatherService.ts
+import { getBaseTime } from '../utils/timeUtils';
+
 const API_KEY = 'n8uIBadsqMx4UqYvGKL7l7l2Gkut99sQyvUHXQJdNhOo0pQQRE0vwEgMMYsptCZ91a4L%2Fna8hWLVrGmNkOQS5w%3D%3D';
 
 export const getWeatherData = async (date: string, nx: number, ny: number) => {
-  const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${date}&base_time=0200&nx=${nx}&ny=${ny}`;
+  const baseTime = getBaseTime(); // 동적으로 base_time 설정
+  const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${date}&base_time=${baseTime}&nx=${nx}&ny=${ny}`;
 
   try {
     const response = await fetch(url);
 
-    // 응답이 성공적으로 왔는지 확인
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const data = await response.json();
 
-    // 데이터 구조가 올바른지 확인 후 반환
     if (data && data.response && data.response.body && data.response.body.items) {
       return data.response.body.items.item;
     } else {
