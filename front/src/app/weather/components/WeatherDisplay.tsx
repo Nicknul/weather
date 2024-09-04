0622e822a6c86e6a1b600a2ff5f2dd295c8a9e8e
정호연 <jea7730@gmail.com>
2024-09-04 20:23:51 +0900

    :sparkles: 정호연 : 모든 날짜의 TMP 데이터 추출

--- front/src/app/weather/components/WeatherDisplay.tsx
+++ front/src/app/weather/components/WeatherDisplay.tsx
@@ -11,25 +11,21 @@ const WeatherDisplay = ({ weatherData }: { weatherData: any[] }) => {
   const tmxData = weatherData.filter((item) => item.category === 'TMX');
   const tmnData = weatherData.filter((item) => item.category === 'TMN');
 
-  // 오늘의 시간별 기온 데이터 필터링
-  const hourlyData = weatherData.filter((item) => item.category === 'TMP' && item.fcstDate === currentDate);
+  // 시간별 기온 데이터 필터링 (TMP)
+  const hourlyData = weatherData.filter((item) => item.category === 'TMP');
 
   return (
     <div>
       {tmxData.map((tmx, index) => {
         const tmn = tmnData[index];
-        const apiDate = tmx.fcstDate;
-
-        // 날짜가 오늘인지 확인
-        const isToday = apiDate === currentDate;
 
         return (
           <div key={index}>
-            <WeatherDay data={[tmx, tmn]} /> {/* 최고/최저 기온 표시 */}
-            {isToday && <WeatherHour hourlyData={hourlyData} />} {/* 오늘의 시간별 기온 표시 */}
+            <WeatherDay data={[tmx, tmn]} /> {/* 날짜별 최고/최저 기온 표시 */}
           </div>
         );
       })}
+      <WeatherHour hourlyData={hourlyData} /> {/* 시간별 기온 데이터 표시 */}
     </div>
   );
 };
