29e165b86465d4c947f66b9f7efe2fe3125aa2c8
정호연 <jea7730@gmail.com>
2024-09-04 14:42:50 +0900

    :sparkles: 정호연 : 내일, 모레, 글피 값 나타나도록 함

--- storage/public/js/date.js
+++ storage/public/js/date.js
@@ -1,6 +1,17 @@
 window.addEventListener('load', () => {
-  const now = new Date();
+  /**
+   * 문사 요소 불러오기
+   */
+  const hour = document.getElementById('hour-bundle');
+  const today = document.getElementById('today-bundle');
+  const tomorrow = document.getElementById('tomorrow-bundle');
+  const DayAfterTomorrow = document.getElementById('day-after-tomorrow-bundle');
+  const TwoDaysAfterTomorrow = document.getElementById('two-day-after-tomorrow-bundle');
 
+  /**
+   * 현재 날짜 추출
+   */
+  const now = new Date();
   let year = now.getFullYear();
   let month = (now.getMonth() + 1).toString();
   let date = now.getDate().toString();
@@ -19,25 +30,11 @@ window.addEventListener('load', () => {
   const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${key}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${processDate}&base_time=0200&nx=67&ny=100`;
   // console.log(url);
 
-  const root = document.getElementById('root');
-
   async function shortWeather() {
     let a = await fetch(url, { method: 'GET' });
     let b = await a.json();
 
     const data = b.response.body.items.item; // 단기예보 데이터
-
-    const set = document.createElement('div');
-    const todaySets = document.createElement('div');
-    const hourlySets = document.createElement('div');
-
-    set.id = 'set';
-    todaySets.id = 'today';
-    hourlySets.id = 'hourlySets';
-
-    set.appendChild(hourlySets);
-    set.appendChild(todaySets);
-    root.appendChild(set);
 
     for (let i = 0; i < data.length; i++) {
       let category = data[i].category;
@@ -63,7 +60,7 @@ window.addEventListener('load', () => {
             if (categoryArr[j] === 'TMX') {
               let highest = document.createElement('div');
               highest.textContent = `${fcstValue}`;
-              todaySets.appendChild(highest);
+              today.appendChild(highest);
             } else if (categoryArr[j] === 'TMN') {
               let days = document.createElement('div');
               let lowest = document.createElement('div');
@@ -71,8 +68,8 @@ window.addEventListener('load', () => {
               days.textContent = '오늘';
               lowest.textContent = `${fcstValue}℃`;
 
-              todaySets.appendChild(days);
-              todaySets.appendChild(lowest);
+              today.appendChild(days);
+              today.appendChild(lowest);
             } else {
               /**
                * hour : 시간
@@ -83,30 +80,98 @@ window.addEventListener('load', () => {
               let b = Number(a);
 
               let temperature = document.createElement('div');
-              let hour = document.createElement('div');
+              let hours = document.createElement('div');
               let hourly = document.createElement('div');
 
               hourly.className = 'hourly';
-              hour.textContent = `${b}시`;
+              hours.textContent = `${b}시`;
               temperature.textContent = `${fcstValue}℃`;
 
-              hourly.appendChild(hour);
+              hourly.appendChild(hours);
               hourly.appendChild(temperature);
-              hourlySets.appendChild(hourly);
+              hour.appendChild(hourly);
             }
-          } else {
-            let itemYear = fcstDate.slice(0, 4);
-            let itemMonth = fcstDate.slice(4, 6);
-            let itemDay = fcstDate.slice(6, 8);
+          } else if (fcstDate === `${transformDate + 1}`) {
+            if (categoryArr[j] === 'TMX') {
+              let highest = document.createElement('div');
+              highest.textContent = `${fcstValue}`;
+              tomorrow.appendChild(highest);
+            } else if (categoryArr[j] === 'TMN') {
+              let days = document.createElement('div');
+              let lowest = document.createElement('div');
 
-            let a = `${itemYear}-${itemMonth}-${itemDay}`;
+              let itemYear = fcstDate.slice(0, 4);
+              let itemMonth = fcstDate.slice(4, 6);
+              let itemDay = fcstDate.slice(6, 8);
 
-            let b = new Date(a);
-            let dayIndex = b.getDay();
-            let dayArr = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
-            let days = dayArr[dayIndex];
-            let c = `${days} ${fcstTime} = ${categoryArr[j]} : ${fcstValue}`;
-            console.log(c);
+              let a = `${itemYear}-${itemMonth}-${itemDay}`;
+
+              let b = new Date(a);
+              let dayIndex = b.getDay();
+              let dayArr = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+              let c = `${days} ${fcstTime} = ${categoryArr[j]} : ${fcstValue}`;
+              // console.log(c);
+
+              days.textContent = dayArr[dayIndex];
+              lowest.textContent = `${fcstValue}℃`;
+
+              tomorrow.appendChild(days);
+              tomorrow.appendChild(lowest);
+            }
+          } else if (fcstDate === `${transformDate + 2}`) {
+            if (categoryArr[j] === 'TMX') {
+              let highest = document.createElement('div');
+              highest.textContent = `${fcstValue}`;
+              DayAfterTomorrow.appendChild(highest);
+            } else if (categoryArr[j] === 'TMN') {
+              let days = document.createElement('div');
+              let lowest = document.createElement('div');
+
+              let itemYear = fcstDate.slice(0, 4);
+              let itemMonth = fcstDate.slice(4, 6);
+              let itemDay = fcstDate.slice(6, 8);
+
+              let a = `${itemYear}-${itemMonth}-${itemDay}`;
+
+              let b = new Date(a);
+              let dayIndex = b.getDay();
+              let dayArr = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+              let c = `${days} ${fcstTime} = ${categoryArr[j]} : ${fcstValue}`;
+              // console.log(c);
+
+              days.textContent = dayArr[dayIndex];
+              lowest.textContent = `${fcstValue}℃`;
+
+              DayAfterTomorrow.appendChild(days);
+              DayAfterTomorrow.appendChild(lowest);
+            }
+          } else if (fcstDate === `${transformDate + 3}`) {
+            if (categoryArr[j] === 'TMX') {
+              let highest = document.createElement('div');
+              highest.textContent = `${fcstValue}`;
+              TwoDaysAfterTomorrow.appendChild(highest);
+            } else if (categoryArr[j] === 'TMN') {
+              let days = document.createElement('div');
+              let lowest = document.createElement('div');
+
+              let itemYear = fcstDate.slice(0, 4);
+              let itemMonth = fcstDate.slice(4, 6);
+              let itemDay = fcstDate.slice(6, 8);
+
+              let a = `${itemYear}-${itemMonth}-${itemDay}`;
+
+              let b = new Date(a);
+              let dayIndex = b.getDay();
+              let dayArr = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+              let c = `${days} ${fcstTime} = ${categoryArr[j]} : ${fcstValue}`;
+              // console.log(c);
+
+              days.textContent = dayArr[dayIndex];
+              lowest.textContent = `${fcstValue}℃`;
+
+              TwoDaysAfterTomorrow.appendChild(days);
+              TwoDaysAfterTomorrow.appendChild(lowest);
+            }
           }
         }
       }
