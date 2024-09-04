25457539b7c5bbb7e64ef1ff74a59f3846cc89a0
정호연 <jea7730@gmail.com>
2024-09-04 23:13:41 +0900

    :green_heart: 정호연 : Google Maps API Load 조립

--- front/src/app/weather/Weather.tsx
+++ front/src/app/weather/Weather.tsx
@@ -1,11 +1,16 @@
-// weather/Weather.tsx
 'use client';
 import React from 'react';
 import { useWeatherData } from '../hooks/useWeatherData';
 import WeatherDisplay from './components/WeatherDisplay';
+import { useGeocoder } from '../hooks/useGeocoder';
+import { useGoogleMapsLoader } from '../hooks/useGoogleMapsLoader'; // Google Maps 로드 훅 추가
 
 const Weather = () => {
-  const { weatherData, locationError, error, isLoading } = useWeatherData();
+  const { weatherData, location, locationError, error, isLoading } = useWeatherData();
+  const geocoderResult = useGeocoder(location?.lat ?? null, location?.lon ?? null);
+
+  // Google Maps 스크립트 로드
+  useGoogleMapsLoader(); // 스크립트 로드 훅 호출
 
   return (
     <div>
@@ -17,6 +22,15 @@ const Weather = () => {
       ) : (
         weatherData.length > 0 && <WeatherDisplay weatherData={weatherData} />
       )}
+
+      {/* 구글 지오코딩 결과 표시 */}
+      {geocoderResult.address && (
+        <div>
+          <h2>현재 주소</h2>
+          <p>{geocoderResult.address}</p>
+        </div>
+      )}
+      {geocoderResult.error && <p>{geocoderResult.error}</p>}
     </div>
   );
 };
