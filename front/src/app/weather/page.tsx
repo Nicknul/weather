5f5e97c982feec968c0ef2bb8e7829f699442793
정호연 <jea7730@gmail.com>
2024-09-04 17:16:22 +0900

    :recycle: 정호연 : 컴포넌트, 모듈 조립

--- front/src/app/weather/page.tsx
+++ front/src/app/weather/page.tsx
@@ -1,12 +1,22 @@
+// weather/Weather.tsx
 'use client';
-import WeatherButton from './components/WeatherButton';
+import React from 'react';
+import { useWeather } from '../hooks/useWeather';
+import WeatherDisplay from './components/WeatherDisplay';
 
-export default function Weather() {
+const Weather = () => {
+  window.addEventListener('load', () => {
+    const now = new Date();
+    console.log(now);
+  });
+  const weatherData = useWeather('20240904'); // 현재 날짜 예시
+
   return (
-    <>
-      <div className="m-5 bg-blue">
-        <WeatherButton />
-      </div>
-    </>
+    <div>
+      <h1>날씨 정보</h1>
+      {weatherData.length > 0 ? <WeatherDisplay weatherData={weatherData} /> : <div>데이터를 불러오는 중...</div>}
+    </div>
   );
-}
+};
+
+export default Weather;
